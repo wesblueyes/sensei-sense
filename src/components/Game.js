bbe85da16eb903c521aadc26e8e01b81484ec797
Jason L Perry <jasper@ambethia.com>
2016-09-07 15:04:47 -0400

    Some design

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,5 +1,41 @@
 import React from 'react'
 
 export default () => {
-  return <h2>Game</h2>
+  return <div className='game'>
+    <div className='previous'>
+      <div className='turn'>
+        <div className='cup' />
+        <div className='cup' />
+        <div className='cup' />
+        <div className='cup' />
+        <div className='pegs' />
+      </div>
+      <div className='turn'>
+        <div className='cup' />
+        <div className='cup' />
+        <div className='cup' />
+        <div className='cup' />
+        <div className='pegs' />
+      </div>
+    </div>
+    <div className='current turn'>
+      <div className='cup' />
+      <div className='cup' />
+      <div className='cup' />
+      <div className='cup' />
+      <div className='go'>
+        <button>
+          Go
+        </button>
+      </div>
+    </div>
+    <div className='color-well'>
+      <div className='color green' />
+      <div className='color aqua' />
+      <div className='color fuschia' />
+      <div className='color blueberry' />
+      <div className='color fire' />
+      <div className='color coal' />
+    </div>
+  </div>
 }
--- src/components/Title.js
+++ src/components/Title.js
@@ -1,13 +1,12 @@
 import React from 'react'
+import { Link } from 'react-router'
 
 export default () => {
   return <div>
     <h2>Title</h2>
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+
+    <p>TODO: Describe how to play the game and make a play button</p>
+
+    <button><Link to='/game'>PLAY!</Link></button>
   </div>
 }
